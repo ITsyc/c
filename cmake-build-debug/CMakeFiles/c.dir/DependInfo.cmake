c564ef54e5feea8d98103c52c8a1e22630087b5e
songyichao <songyichao@mioji.com>
2017-01-24 11:18:21 +0800

    ex 18

---
+++ cmake-build-debug/CMakeFiles/c.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Library/WebServer/Documents/c/ex10.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex10.c.o"
+  "/Library/WebServer/Documents/c/ex11.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex11.c.o"
+  "/Library/WebServer/Documents/c/ex12.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex12.c.o"
+  "/Library/WebServer/Documents/c/ex13.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex13.c.o"
+  "/Library/WebServer/Documents/c/ex14.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex14.c.o"
+  "/Library/WebServer/Documents/c/ex15.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex15.c.o"
+  "/Library/WebServer/Documents/c/ex16.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex16.c.o"
+  "/Library/WebServer/Documents/c/ex17.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex17.c.o"
+  "/Library/WebServer/Documents/c/ex18.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex18.c.o"
+  "/Library/WebServer/Documents/c/ex3.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex3.c.o"
+  "/Library/WebServer/Documents/c/ex6.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex6.c.o"
+  "/Library/WebServer/Documents/c/ex7.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex7.c.o"
+  "/Library/WebServer/Documents/c/ex8.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex8.c.o"
+  "/Library/WebServer/Documents/c/ex9.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/ex9.c.o"
+  "/Library/WebServer/Documents/c/test1.c" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/test1.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Library/WebServer/Documents/c/main.cpp" "/Library/WebServer/Documents/c/cmake-build-debug/CMakeFiles/c.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
